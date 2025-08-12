c262eab70cb31a518b8b7e65464bc027fddfa8ea
bernat <bernat@grupoindieprof.com>
2025-08-12 23:23:23 +0200

    cambios de configuracon para tener themes

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ const dev = process.env.ROLLUP_WATCH;
 export default {
   input: "src/main.ts",
   output: {
-    file: "neumorphism-cards.js",
+    file: "dist/neumorphism-cards.js",
     format: "es",
     sourcemap: true,
   },
